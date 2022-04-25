26d573c96ef27d33060f9f1e7c78d8a0d097e626
Joy <joyjk99@gmail.com>
2022-04-25 15:03:05 +0600

    Updated Using service and interface service

--- ExamSystemAppApi/Controllers/UsersController.cs
+++ ExamSystemAppApi/Controllers/UsersController.cs
@@ -20,47 +20,26 @@ namespace ExamSystemAppApi.Controllers
         private readonly IUserServices userService;
         SQL _sqlDal = null;
         string msg = "";
-        //private readonly ExamSystemEFContext examSystemEFContext;
-
-        //public UsersController(ExamSystemEFContext examSystemEFContext)
-        //{
-        //    this.examSystemEFContext = examSystemEFContext;
-        //}
-
-        public UsersController(ExamSystemContext examSystemContext, IConfiguration configuration, IUserServices userService)
+       
+        public UsersController(IUserServices userService)
         {
-            this.examSystemContext = examSystemContext;
-            this.configuration = configuration;
+            
             this.userService = userService;
-            _sqlDal = new SQL(configuration.GetConnectionString("DefaultConnection"));
+           
         }
 
         [HttpGet]
         public IActionResult Get()
         {
-            ////User users = _sqlDal.Select("select * from users", ref msg);
-            string q = string.Format(@"select * from users");
-            var users = _sqlDal.Select<User>(q,ref msg);
-            //userService.GetAllUsers();
-
 
             return Ok(userService.GetAllUsers());
-            //return Ok(examSystemContext.QuizOptions.ToList()); ;
-            //return Ok(examSystemEFContext.Users.ToList());
-            //string query = string.Format(@"select a.admission_date, a.invoice_id from hospital.admission a where a.admission_id = '" + obj.aAdmissionServiceDetails.admission_id + "'";
-           // var aAdmission = _sqlDal.SelectFirstOrDefault<Admission>(query, ref msg);
-
         }
         [HttpPost]
         public IActionResult Post(User user)
         {
-            //examSystemContext.Users.Add(user);
-            //examSystemContext.SaveChanges();
+            
 
-            var userList = new List<User>();
-            userList.Add(user);
-
-            _sqlDal.Insert<User>(userList,"","Id","Users",ref msg);
+            userService.InsertNewUser(user);
 
 
             return Created("created", user);
@@ -68,21 +47,15 @@ namespace ExamSystemAppApi.Controllers
         [HttpPut]
         public IActionResult Put(User user)
         {
-            //user.Id = id;
-            //examSystemContext.Users.Update(user);
-            //examSystemContext.SaveChanges();
-            _sqlDal.Update<User>(user, "", "id", "Users", ref msg);
+          
+            userService.UpdateUser(user);
             return Ok();
         }
         [HttpDelete("{id:int}")]
         public IActionResult Delete(int id)
         {
-            //var user = examSystemContext.Users.Find(id);
-            var query = "select * from Users where id=" + id;
-            var user = _sqlDal.SelectFirstOrDefault<User>(query,ref msg);
-            //examSystemContext.Users.Remove(user);
-            //examSystemContext.SaveChanges();
-            _sqlDal.Delete<User>(user, "Id", "Users", ref msg);
+           
+            userService.DeleteUser(id);
             return NoContent();
         }
     }
